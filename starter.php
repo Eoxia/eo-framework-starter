0ddf6ddd075bae3b2a996b47a233afc5801ac8f2
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-11-09 10:11:21 +0100

    0.1.0: Mise à jour Doc

--- starter.php
+++ starter.php
@@ -1,24 +1,24 @@
 <?php
 /*
 Plugin Name:  EO Framework Starter
-Plugin URI:   https://developer.wordpress.org/plugins/the-basics/
-Description:  Un plugin WordPress utilisant EO-Framework.
+Plugin URI:
+Description:  Un plugin "Starter" WordPress utilisant EO-Framework.
 Version:      0.1.0
 Author:       Eoxia
 Author URI:   https://eoxia.com
 License:      GPL2
 License URI:  https://www.gnu.org/licenses/gpl-2.0.html
-Text Domain:  eo-starter-domain
-Domain Path:  /languages
 */
 
-/** Des defines utiles pour inclure votre CSS, JS et fichier MO **/
+// Des defines utiles pour inclure votre CSS, JS et fichier MO.
+// *_NOM_PLUGIN_* doit être remplacé par le nom de votre plugin.
 DEFINE( 'PLUGIN_NOM_PLUGIN_PATH', realpath( plugin_dir_path( __FILE__ ) ) . '/' );
 DEFINE( 'PLUGIN_NOM_PLUGIN_URL', plugins_url( basename( __DIR__ ) ) . '/' );
 DEFINE( 'PLUGIN_NOM_PLUGIN_DIR', basename( __DIR__ ) );
 
-/** Ligne supplémentaire pour utiliser eo-framework **/
+// Ligne supplémentaire pour utiliser eo-framework.
 require_once( 'core/external/eo-framework/eo-framework.php' );
 
-/** Boot votre plugin; Nous reviendrons sur cette ligne dans les prochains chapitres. **/
+// Boot votre plugin
+// @see https://github.com/Eoxia/eo-framework pour en savoir plus.
 \eoxia\Init_Util::g()->exec( PLUGIN_NOM_PLUGIN_PATH, basename( __FILE__, '.php' ) );
