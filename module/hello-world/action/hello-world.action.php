a12b3339193a240dcde71df6c652ed960469aff4
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-11-09 10:11:05 +0100

    0.1.0: Ajout Doc

--- module/hello-world/action/hello-world.action.php
+++ module/hello-world/action/hello-world.action.php
@@ -1,13 +1,28 @@
 <?php
+/**
+ * Les actions du module "Hello World".
+ *
+ * @author Jimmy Latour <jimmy.eoxia@gmail.com>
+ * @since 0.1.0
+ * @version 0.1.0
+ * @copyright 2015-2017 Eoxia
+ * @package EO_Framework_Starter
+ */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+/**
+ * Les actions du module "Hello World".
+ */
 class Hello_World_Action {
 
 	/**
 	 * Le constructeur
+	 *
+	 * @since 0.1.0
+	 * @version 0.1.0
 	 */
 	public function __construct() {
 		add_action( 'admin_menu', array( $this, 'callback_admin_menu' ) );
@@ -16,6 +31,9 @@ class Hello_World_Action {
 
 	/**
 	 * Ajout du sous menu 'Hello World' dans le menu de WordPress.
+	 *
+	 * @since 0.1.0
+	 * @version 0.1.0
 	 */
 	public function callback_admin_menu() {
 		add_menu_page( 'Hello World', 'Hello World', 'manage_options', 'hello-world', array( $this, 'callback_add_menu_page' ) );
@@ -23,6 +41,9 @@ class Hello_World_Action {
 
 	/**
 	 * Le callback pour afficher la vue.
+	 *
+	 * @since 0.1.0
+	 * @version 0.1.0
 	 */
 	public function callback_add_menu_page() {
 		\eoxia\View_Util::exec( 'starter', 'hello_world', 'main' );
--- module/hello-world/asset/js/hello-world.backend.js
+++ module/hello-world/asset/js/hello-world.backend.js
@@ -1,21 +1,40 @@
-window.eoxiaJS.test.helloWorld = {};
-window.eoxiaJS.test.helloWorld.init = function() {
-	window.eoxiaJS.test.helloWorld.event();
+/**
+ * Initialise l'objet helloWorld dans le namespace eoFrameworkStarter.
+ * Permet d'éviter les conflits entre les différents plugins utilisant EO-Framework.
+ *
+ * Ce fichier JS est une base pour utiliser du JS avec EO-Framework.
+ * En lançant la commande "npm start", GULP vas écouter les fichiers *.backend.js et
+ * vas s'occuper de les assembler dans le fichier backend.min.js.
+ *
+ * EO-Framework appel automatiquement la méthode "init" à l'initilisation de certains *pages*
+ * du backadmin de WordPress. Ces pages doivent être définis dans le tableau "insert_scripts_page" dans le fichier *.config.json
+ * principales de votre plugin.
+ * @see https://github.com/Eoxia/task-manager/blob/master/task-manager.config.json pour un exemple
+ *
+ * @since 0.1.0
+ * @version 0.1.0
+ */
+window.eoxiaJS.eoFrameworkStarter.helloWorld = {};
+
+/**
+ * La méthode "init" est appelé automatiquement par la lib JS de Eo-Framework
+ *
+ * @since 0.1.0
+ * @version 0.1.0
+ *
+ * @return {void}
+ */
+window.eoxiaJS.eoFrameworkStarter.helloWorld.init = function() {
+	window.eoxiaJS.eoFrameworkStarter.helloWorld.event();
 };
 
-window.eoxiaJS.test.helloWorld.event = function() {
-	jQuery( document ).on( 'mouseenter', '.wpeo-tooltip', function( event ) {
-		var el = jQuery( '<span class="test">' + jQuery( this ).attr( 'aria-label' ) + '</span>' );
-		var pos = jQuery( this ).offset();
-		jQuery( this )[0].tooltipElement = el;
-		jQuery( 'body' ).append( jQuery( this )[0].tooltipElement );
-		el.css( {
-			'transform': 'translate3d(' + pos.left + 'px, ' + ( pos.top - jQuery( this ).height() - el.height() ) + 'px, 0px)',
-			'opacity': 1
-		} );
-	} );
-
-	jQuery( document ).on( 'mouseleave', '.wpeo-tooltip', function( event ) {
-		jQuery( jQuery( this )[0].tooltipElement ).remove();
-	} );
-};
+/**
+ * La méthode "event" est appelé par la méthode "init".
+ * Elle n'est pas obligatoire mais nous avons comme habitude de mêttre les évènements dans celle-ci. (addEventListener)
+ *
+ * @since 0.1.0
+ * @version 0.1.0
+ *
+ * @return {void}
+ */
+window.eoxiaJS.eoFrameworkStarter.helloWorld.event = function() {};
