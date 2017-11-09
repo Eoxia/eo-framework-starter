765cdd3b49463fb9f6ee6d35c4bc0d30e87ee135
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-11-09 10:10:27 +0100

    0.1.0: Norme WordPress

--- core/action/core.action.php
+++ core/action/core.action.php
@@ -3,10 +3,10 @@
  * Les actions principales de l'application.
  *
  * @author Jimmy Latour <jimmy.eoxia@gmail.com>
- * @since 1.0.0
- * @version 1.5.1
+ * @since 0.1.0
+ * @version 0.1.0
  * @copyright 2015-2017 Eoxia
- * @package Task_Manager
+ * @package EO_Framework_Starter
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -16,36 +16,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Les actions principales de l'application.
  */
-class Test_Action {
+class Core_Action {
 
 	/**
-	 * Le constructeur ajoutes les actions WordPress suivantes:
-	 * admin_enqueue_scripts (Pour appeller les scripts JS et CSS dans l'admin)
-	 * admin_print_scripts (Pour appeler les scripts JS en bas du footer)
-	 * plugins_loaded (Pour appeler le domaine de traduction)
+	 * Le constructeur
+	 *
+	 * @since 0.1.0
+	 * @version 0.1.0
 	 */
 	public function __construct() {
 		add_action( 'admin_enqueue_scripts', array( $this, 'callback_admin_enqueue_scripts' ), 11 );
 	}
 
 	/**
-	 * Initialise les fichiers JS inclus dans WordPress (jQuery, wp.media et thickbox)
-	 *
-	 * @return void nothing
-	 */
-	public function callback_before_admin_enqueue_scripts() {
-		wp_enqueue_script( 'jquery' );
-		wp_enqueue_script( 'jquery-form' );
-		wp_enqueue_script( 'jquery-ui-datepicker' );
-		wp_enqueue_script( 'jquery-ui-sortable' );
-		wp_enqueue_script( 'jquery-ui-accordion' );
-		wp_enqueue_script( 'jquery-ui-autocomplete' );
-		wp_enqueue_media();
-		add_thickbox();
-	}
-
-	/**
 	 * Initialise le fichier style.min.css et backend.min.js du plugin DigiRisk.
+	 *
+	 * @since 0.1.0
+	 * @version 0.1.0
 	 *
 	 * @return void nothing
 	 */
@@ -55,4 +42,4 @@ class Test_Action {
 	}
 }
 
-new Test_Action();
+new Core_Action();
